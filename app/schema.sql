c99146eff3805a6ea148bda50454c16fc78804f7
IreniaYulieth-94 <113447207+IreniaYulieth-94@users.noreply.github.com>
2022-10-12 19:34:46 -0500

    Update schema.sql

--- app/schema.sql
+++ app/schema.sql
@@ -52,4 +52,4 @@ CREATE TABLE credentials(
   password TEXT NOT NULL
 );
 
-INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','angelicamt@uninorte.edu.co', 'David2009');
+INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','irenias@uninorte.edu.co', 'JwGot794995');
